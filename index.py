38cb06540e3b48cf3c75613e6cb17c946beebf1c
EC2 Default User <ec2-user@ip-172-31-83-243.ec2.internal>
2020-03-08 15:49:11 +0000

    time test

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World 1',
+        'output': 'Hello World 11',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
