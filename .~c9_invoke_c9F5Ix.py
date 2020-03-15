b7457e666605cb526c8b5b9eb0dfa51244bc394b
EC2 Default User <ec2-user@ip-172-31-86-82.ec2.internal>
2020-03-15 14:46:12 +0000

    adding attachment

--- .~c9_invoke_c9F5Ix.py
+++ .~c9_invoke_c9F5Ix.py
@@ -5,7 +5,11 @@ import numpy as np
 import datetime
 import boto3
 import json
+import os
 from dateutil import tz
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
 
 from talib import EMA,SMA, WMA
 # from finta import TA
@@ -82,71 +86,100 @@ class YahooFinance:
     def to_csv(self, file_name):
         self.result.to_csv(file_name)
 
-def sendAWSMail(recipeint,subject,body_text):
+def sendAWSMailAttachment(recipeint,subject,body_text,attachPath):
     # Replace sender@example.com with your "From" address.
     # This address must be verified with Amazon SES.
     SENDER = "dartconsultants.hyd@gmail.com"
-    # Replace recipient@example.com with a "To" address. If your account
+    
+    # Replace recipient@example.com with a "To" address. If your account 
     # is still in the sandbox, this address must be verified.
     RECIPIENT = recipeint
+    
     # Specify a configuration set. If you do not want to use a configuration
-    # set, comment the following variable, and the
+    # set, comment the following variable, and the 
     # ConfigurationSetName=CONFIGURATION_SET argument below.
     # CONFIGURATION_SET = "ConfigSet"
+    
     # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
-    AWS_REGION = "us-west-2"
+    AWS_REGION = "us-east-1"
+    
     # The subject line for the email.
     SUBJECT = subject
-
+    
+    # The full path to the file that will be attached to the email.
+    ATTACHMENT = attachPath
+    
     # The email body for recipients with non-HTML email clients.
     BODY_TEXT = body_text
-
-    # The HTML body of the email.
-    # BODY_HTML = """<html>
+    
+    # # The HTML body of the email.
+    # BODY_HTML = """\
+    # <html>
     # <head></head>
     # <body>
-    #   <h1>Amazon SES Test (SDK for Python)</h1>
-    #   <p>This email was sent with
-    #     <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
-    #     <a href='https://aws.amazon.com/sdk-for-python/'>
-    #       AWS SDK for Python (Boto)</a>.</p>
+    # <h1>Hello!</h1>
+    # <p>Please see the attached file for a list of customers to contact.</p>
     # </body>
     # </html>
-    #             """
+    # """
+    
     # The character encoding for the email.
-    # Test comment to check in
-    CHARSET = "UTF-8"
+    CHARSET = "utf-8"
+    
     # Create a new SES resource and specify a region.
-    client = boto3.client('ses', region_name=AWS_REGION)
-    # Try to send the email.
+    client = boto3.client('ses',region_name=AWS_REGION)
+    
+    # Create a multipart/mixed parent container.
+    msg = MIMEMultipart('mixed')
+    # Add subject, from and to lines.
+    msg['Subject'] = SUBJECT 
+    msg['From'] = SENDER 
+    msg['To'] = RECIPIENT
+    
+    # Create a multipart/alternative child container.
+    msg_body = MIMEMultipart('alternative')
+    
+    # Encode the text and HTML content and set the character encoding. This step is
+    # necessary if you're sending a message with characters outside the ASCII range.
+    textpart = MIMEText(BODY_TEXT.encode(CHARSET), 'plain', CHARSET)
+    # htmlpart = MIMEText(BODY_HTML.encode(CHARSET), 'html', CHARSET)
+    
+    # Add the text and HTML parts to the child container.
+    msg_body.attach(textpart)
+    # msg_body.attach(htmlpart)
+    
+    # Define the attachment part and encode it using MIMEApplication.
+    att = MIMEApplication(open(ATTACHMENT, 'rb').read())
+    
+    # Add a header to tell the email client to treat this part as an attachment,
+    # and to give the attachment a name.
+    att.add_header('Content-Disposition','attachment',filename=os.path.basename(ATTACHMENT))
+    
+    # Attach the multipart/alternative child container to the multipart/mixed
+    # parent container.
+    msg.attach(msg_body)
+    
+    # Add the attachment to the parent container.
+    msg.attach(att)
+    #print(msg)
     try:
-        # Provide the contents of the email.
-        response = client.send_email(
-            Destination={
-                'ToAddresses': [
-                    RECIPIENT,
-                ],
-            },
-            Message={
-                'Body': {
-                    'Text': {
-                        'Charset': CHARSET,
-                        'Data': BODY_TEXT,
-                    },
-                },
-                'Subject': {
-                    'Charset': CHARSET,
-                    'Data': SUBJECT,
-                },
-            },
+        #Provide the contents of the email.
+        response = client.send_raw_email(
             Source=SENDER,
-            # If you are not using a configuration set, comment or delete the
-            # following line
-            # ConfigurationSetName=CONFIGURATION_SET,
+            Destinations=[
+                RECIPIENT
+            ],
+            RawMessage={
+                'Data':msg.as_string(),
+            }
+            # ConfigurationSetName=CONFIGURATION_SET
         )
-    # Display an error if something goes wrong.
+    # Display an error if something goes wrong.	
     except ClientError as e:
         print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])    
 
 
 
@@ -321,7 +354,7 @@ def handler(event, context):
     BODY_TEXT = BODY_TEXT + "\n\n" + "List of stocks below 200 SMA and crossed 200 SMA"
     BODY_TEXT = BODY_TEXT + "\n\n" + "\n".join(below2002percentCross['Symbol'].tolist()[0:])
     # print(BODY_TEXT)
-    sendAWSMail('dartconsultants.hyd@gmail.com',SUBJECT,BODY_TEXT)
+    sendAWSMailAttachment('dartconsultants.hyd@gmail.com',SUBJECT,BODY_TEXT,tmpPath)
     # sendAWSMail(SUBJECT, BODY_TEXT)
     
     # above2002percentNoCross['Symbol'].series.to_csv('/home/ec2-user/environment/dart-stock-scan-lambda/above2002percentNoCross')
