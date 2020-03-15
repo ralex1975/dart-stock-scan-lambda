60e182453759d8dafccdb1187204e8214befc698
EC2 Default User <ec2-user@ip-172-31-86-82.ec2.internal>
2020-03-15 12:15:58 +0000

    first actual working version try

--- index.py
+++ index.py
@@ -82,6 +82,73 @@ class YahooFinance:
     def to_csv(self, file_name):
         self.result.to_csv(file_name)
 
+def sendAWSMail(recipeint,subject,body_text):
+    # Replace sender@example.com with your "From" address.
+    # This address must be verified with Amazon SES.
+    SENDER = "dartconsultants.hyd@gmail.com"
+    # Replace recipient@example.com with a "To" address. If your account
+    # is still in the sandbox, this address must be verified.
+    RECIPIENT = recipeint
+    # Specify a configuration set. If you do not want to use a configuration
+    # set, comment the following variable, and the
+    # ConfigurationSetName=CONFIGURATION_SET argument below.
+    # CONFIGURATION_SET = "ConfigSet"
+    # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
+    AWS_REGION = "us-west-2"
+    # The subject line for the email.
+    SUBJECT = subject
+
+    # The email body for recipients with non-HTML email clients.
+    BODY_TEXT = body_text
+
+    # The HTML body of the email.
+    # BODY_HTML = """<html>
+    # <head></head>
+    # <body>
+    #   <h1>Amazon SES Test (SDK for Python)</h1>
+    #   <p>This email was sent with
+    #     <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+    #     <a href='https://aws.amazon.com/sdk-for-python/'>
+    #       AWS SDK for Python (Boto)</a>.</p>
+    # </body>
+    # </html>
+    #             """
+    # The character encoding for the email.
+    # Test comment to check in
+    CHARSET = "UTF-8"
+    # Create a new SES resource and specify a region.
+    client = boto3.client('ses', region_name=AWS_REGION)
+    # Try to send the email.
+    try:
+        # Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    RECIPIENT,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER,
+            # If you are not using a configuration set, comment or delete the
+            # following line
+            # ConfigurationSetName=CONFIGURATION_SET,
+        )
+    # Display an error if something goes wrong.
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+
+
 
 def fetch_data(symbol, period, interval):
     pd.options.display.max_rows = 2000
@@ -230,10 +297,26 @@ def main():
     above2002percentCross = outputDf[(outputDf['Close'] >= outputDf['200SMA']) & (outputDf['Close'] <= 1.02 * outputDf['200SMA']) & (outputDf['ARB-200SMA'] != outputDf['ARB-200SMAprev1'])]
     below2002percentCross = outputDf[(outputDf['Close'] < outputDf['200SMA']) & (outputDf['Close'] >= 0.98 * outputDf['200SMA']) & (outputDf['ARB-200SMA'] != outputDf['ARB-200SMAprev1'])]    
     
-    above2002percentNoCross['Symbol'].to_csv('/home/ec2-user/environment/dart-stock-scan-lambda/above2002percentNoCross')
-    below2002percentNoCross['Symbol'].to_csv('/home/ec2-user/environment/dart-stock-scan-lambda/below2002percentNoCross')
-    above2002percentCross['Symbol'].to_csv('/home/ec2-user/environment/dart-stock-scan-lambda/above2002percentCross')
-    below2002percentCross['Symbol'].to_csv('/home/ec2-user/environment/dart-stock-scan-lambda/below2002percentCross')
+    # print(above2002percentNoCross['Symbol'].tolist())
+    # print("\n".join(above2002percentNoCross['Symbol'].tolist()[0:]))
+    
+    SUBJECT = "Daily stock tracker"
+    # The email body for recipients with non-HTML email clients.
+    BODY_TEXT = "List of stocks aobve 200 SMA - within 2 percent"
+    BODY_TEXT = BODY_TEXT + "\n\n" + "\n".join(above2002percentNoCross['Symbol'].tolist()[0:])
+    BODY_TEXT = BODY_TEXT + "\n\n" + "List of stocks below 200 SMA - within 2 percent"
+    BODY_TEXT = BODY_TEXT + "\n\n" + "\n".join(below2002percentNoCross['Symbol'].tolist()[0:])
+    BODY_TEXT = BODY_TEXT + "\n\n" + "List of stocks aobve 200 SMA and corssed 200 SMA"
+    BODY_TEXT = BODY_TEXT + "\n\n" + "\n".join(above2002percentCross['Symbol'].tolist()[0:])
+    BODY_TEXT = BODY_TEXT + "\n\n" + "List of stocks below 200 SMA and crossed 200 SMA"
+    BODY_TEXT = BODY_TEXT + "\n\n" + "\n".join(below2002percentCross['Symbol'].tolist()[0:])
+    sendAWSMail('dartconsultants.hyd@gmail.com',SUBJECT,BODY_TEXT)
+    # sendAWSMail(SUBJECT, BODY_TEXT)
+    
+    # above2002percentNoCross['Symbol'].series.to_csv('/home/ec2-user/environment/dart-stock-scan-lambda/above2002percentNoCross')
+    # below2002percentNoCross['Symbol'].series.to_csv('/home/ec2-user/environment/dart-stock-scan-lambda/below2002percentNoCross')
+    # above2002percentCross['Symbol'].series.to_csv('/home/ec2-user/environment/dart-stock-scan-lambda/above2002percentCross')
+    # below2002percentCross['Symbol'].series.to_csv('/home/ec2-user/environment/dart-stock-scan-lambda/below2002percentCross')
     
     # print(above2002percent)
     # print(below2002percent)
